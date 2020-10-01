6fb633df405c967e0d469b0f4129e0718a399610
anshul0771 <72175375+anshul0771@users.noreply.github.com>
2020-10-01 06:48:25 +0530

    Update Hello-World.sh

--- Hello World Programs/Hello-World.sh
+++ Hello World Programs/Hello-World.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # This is a comment!
 echo "Hello World!"        # This is a comment, too!
+echo "Free T-shirt ke liye kya kya krna padta hai"
+# Hacktoberfest ki free tshirt
+# harr saal 2 tshirt lenge
