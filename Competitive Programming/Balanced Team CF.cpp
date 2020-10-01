424208e19eaec5c49cc4ffb1096731625e32b114
AyushiPanth <lit2019073@iiitl.ac.in>
2020-10-01 19:10:07 +0530

    improved readability of codes

--- Competitive Programming/Balanced Team CF.cpp
+++ Competitive Programming/Balanced Team CF.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int main()
-{
+
+int main() {
     ll n;
-    cin>>n;
+    cin> > n;
     ll a[n];
-    for(ll i=0;i<n;i++)
-      cin>>a[i];
-      sort(a,a+n);
-     ll ptr1=0,ptr2=0,ans=0;
-     for(;ptr2<n;)
-     {
-         if(a[ptr2]-a[ptr1]<=5)
+    for(ll i = 0; i < n; i++) {
+        cin >> a[i];
+      sort(a, a+n);
+      }
+     ll ptr1 = 0, ptr2 = 0, ans = 0;
+
+     for( ; ptr2 < n ; ) {
+         if(a[ptr2] - a[ptr1] <= 5)
           ptr2++;
          else
           ptr1++;
-          if((ptr2-ptr1>ans))
-           ans=ptr2-ptr1;
+          if((ptr2 - ptr1 > ans))
+           ans = ptr2 - ptr1;
      }
-     cout<<ans;
+     cout << ans;
       
 }
  
--- Competitive Programming/simpleCppTemplate.cpp
+++ Competitive Programming/simpleCppTemplate.cpp
@@ -4,12 +4,13 @@
 #include<bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     lli t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while(t--) {
         
     }
     return 0;
