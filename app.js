d45a54d0abe5aa0332c4ad8c08f83cfbc20b3d8e
SHASHANK GUPTA <57327469+thee-moon@users.noreply.github.com>
2020-10-01 11:12:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,25 @@
-const moonPath="M17.5 29C17.5 44.1878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 17.5 13.8122 17.5 29Z";
-
-const sunPath="M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
-
-const darkMode = document.querySelector("#darkMode");
-
-
-var toggle =false;
+var button= document.querySelector('.submit');
+var inputValue = document.querySelector('.inputValue')
+var city =document.querySelector('.city')
+var des =document.querySelector('.des')
+var temp =document.querySelector('.temp')
 
 
+button.addEventListener('click',function(){
 
-darkMode.addEventListener('click',()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=8008395fa13dd591e66311d8b2ee20df`)
+    .then(response => response.json())
+    .then(data => {
+var namevalue=data['name'];
+var tempValue= data['main']['temp']-273;
+var desValue= data['weather'][0]['description'];
 
-const timeline = anime.timeline({
-    duration: 750,
-    easing: "easeOutExpo"
-
-});
-timeline.add({
-    targets: ".sun",
-    d:[
-        {value:toggle?sunPath:moonPath}
-    ]
-})
-.add({
-    targets:"#darkMode",
-    rotate:toggle?0:320
-
-},'-=350')
-.add({
-    targets:"section",
-    backgroundColor: toggle ?"rgb(199,199,199)" :"rgb(22,22,22)"
-},'-=700')
-.add({
-    targets:".head",
-    color:toggle?"rgb(22,22,22)":"rgb(199,199,199)"
-},'-=700');
-
-if(!toggle){
-    toggle=true;
-}else{
-    toggle=false;
-}
+city.innerHTML=namevalue;
+temp.innerHTML=`${tempValue} °F`;
+des.innerHTML=desValue;
+    }
+    )
 
 
+.catch(err =>alert("wrong city name"))
 });
