dae5230144f65fd1765cb6e79c5afd4204d1932c
Pratik Nadgouda <carbonautics@gmail.com>
2020-10-01 21:47:15 +0530

    fixed input bug
    Gave own inputs since github doesn't give any, should run this time

--- Python/Palindrome.py
+++ Python/Palindrome.py
@@ -1,2 +1,4 @@
-my_string = input("Enter a string")
+my_string = 'String'
+print("String is a Palindrome") if my_string.lower()==(my_string.lower())[::-1] else print("String is Not a Palindrome")
+my_string = 'StrrtS'
 print("String is a Palindrome") if my_string.lower()==(my_string.lower())[::-1] else print("String is Not a Palindrome")
