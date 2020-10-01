66c32605b910432b8676a3e3f91d716f34a93cd3
anshul0771 <72175375+anshul0771@users.noreply.github.com>
2020-10-01 06:42:33 +0530

    Update BubbleSort.py

--- Data Structures and Algorithms/Algorithms/BubbleSort.py
+++ Data Structures and Algorithms/Algorithms/BubbleSort.py
@@ -1,25 +1,15 @@
 # Python program for implementation of Bubble Sort
-
-def bubbleSort(arr):
+# Time complexity is O(n^2) which is very slow, prefer the inbuilt sort function over it.
+def sortsort(arr):
     n = len(arr)
-
-    # Traverse through all array elements
     for i in range(n):
-
-        # Last i elements are already in place
         for j in range(0, n-i-1):
-
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+arr = [10, 20, 11, 0, 15, 19]
 
-# Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
+sortsort(arr)
 
-bubbleSort(arr)
-
-print ("Sorted array is:")
+print ("The sorted array is:")
 for i in range(len(arr)):
     print ("%d" %arr[i]), 
