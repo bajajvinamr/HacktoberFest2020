45ce792333f1e4fd57aa6efb83780227999ace27
Pratik Nadgouda <carbonautics@gmail.com>
2020-10-01 21:48:24 +0530

    Delete Palindrome.py

--- Python/Palindrome.py
+++
@@ -1,4 +0,0 @@
-my_string = 'String'
-print("String is a Palindrome") if my_string.lower()==(my_string.lower())[::-1] else print("String is Not a Palindrome")
-my_string = 'StrrtS'
-print("String is a Palindrome") if my_string.lower()==(my_string.lower())[::-1] else print("String is Not a Palindrome")
