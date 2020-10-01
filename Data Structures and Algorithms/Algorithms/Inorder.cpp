83485a2f2cf891af48132fb42adadd08400b5712
Mayank Grover <49029679+MayankG514@users.noreply.github.com>
2020-10-01 23:47:26 +0530

    Update Inorder.cpp

--- Data Structures and Algorithms/Algorithms/Inorder.cpp
+++ Data Structures and Algorithms/Algorithms/Inorder.cpp
@@ -12,6 +12,7 @@ struct Node
         left = right = NULL; 
     } 
 }; 
+
 void printInorder(struct Node* node) 
 { 
     if (node == NULL) 
@@ -26,7 +27,7 @@ int main()
 { 
     struct Node *root = new Node(1); 
     root->left = new Node(2); 
-    root->right = new Node(3); 
+    root->right->left = new Node(3); 
     root->left->left = new Node(4); 
     root->left->right = new Node(5);  
   
