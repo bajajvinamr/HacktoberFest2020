bf29485fde152cc1030f84cf2f5936550557ad42
Shraddhaarora1 <72237247+Shraddhaarora1@users.noreply.github.com>
2020-10-02 06:54:29 +0530

    Create gh.cpp

---
+++ gh.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define ff first
+#define ss second
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repp(i,a,b) for(int i=a;i<=b;i++)
+bool sortt(const pair<int,string> &a,  
+               const pair<int,string> &b) 
+{ 
+       return (a.first > b.first); 
+} 
+signed main()
+{
+    fast
+    int n;
+    cin>>n;
+    int k=n%4;
+    if(k==1){
+        cout<<0<<" "<<"A";
+    }
+    else if(k==0){
+        cout<<1<<" "<<"A";
+    }
+    else if(k==2){
+        cout<<1<<" "<<"B";
+    }
+    else
+    {
+        cout<<2<<" "<<"A";
+    }
+    
+}
