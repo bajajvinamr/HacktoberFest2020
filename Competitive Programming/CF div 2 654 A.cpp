fc402fe82380fe8039ea465b5dcff62fcdf8ca2e
PrajjawalSahu <nikku2001.sahu@gmail.com>
2020-10-03 00:34:25 +0530

    Competetive Programming Codeforces div2 654 A and B

---
+++ Competitive Programming/CF div 2 654 A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        long long int n;
+        cin>>n;
+        cout<<(n+1)/2<<endl;
+    }
+
+    return 0;
+}
