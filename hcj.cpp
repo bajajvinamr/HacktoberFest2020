673001e46963c9bc6d99f86c9ca02c55bee68dae
Shraddhaarora1 <72237247+Shraddhaarora1@users.noreply.github.com>
2020-10-02 06:56:43 +0530

    Create hcj.cpp

---
+++ hcj.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define ff first
+#define ss second
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repp(i,a,b) for(int i=a;i<=b;i++)
+bool sortt(const pair<int,string> &a,  
+               const pair<int,string> &b) 
+{ 
+       return (a.first > b.first); 
+} 
+signed main()
+{
+    fast
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        cout<<2*x<<endl;
+    }
+}
