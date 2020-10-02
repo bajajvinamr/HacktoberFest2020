030e4be7641ebfa200321a7e93521a912ca9e408
Shreyaarora16 <72232427+Shreyaarora16@users.noreply.github.com>
2020-10-02 06:04:37 +0530

    Create op.cpp

---
+++ Competitive Programming/op.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double d,l,x,y;
+    cin>>d>>l>>x>>y;
+    double a=(l-d)/(x+y);
+    cout<<fixed<<setprecision(15);
+    cout<<a<<endl;
+}
