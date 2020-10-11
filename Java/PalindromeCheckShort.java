c64bb5ff881b8088ef45f1f4c35aa0b1aebb985d
ananthalakshmiragi <anantha.lakshmi.ragi@gmail.com>
2020-10-11 21:38:58 +0530

    Create PalindromeCheckShort.java
    Adding a simple code for palindrome check in Java.
    Contributing to Hacktoberfest

---
+++ Java/PalindromeCheckShort.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("seven"));
+        System.out.println(isPalindrome("12"));
+        System.out.println(isPalindrome("mom"));
+        System.out.println(isPalindrome("123321"));
+    }
+    
+    private static boolean isPalindrome(String s) {
+        if (s.equals(new StringBuffer(s).reverse().toString()))
+            return true;
+        else
+            return false;
+    }
+}
