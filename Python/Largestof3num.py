065ec2db50bce2a6e6ebc32b3902de507b356972
Divyanshi Oberoi <72804770+doberoi21@users.noreply.github.com>
2020-10-16 17:16:31 +0530

    Create Largestof3num.py

---
+++ Python/Largestof3num.py
@@ -0,0 +1,12 @@
+num1=20;
+num2=10;
+num3=15;
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
