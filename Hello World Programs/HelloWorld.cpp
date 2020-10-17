9a2136f0418e418bad6f8bef4aae0d461b6b4d17
ayushil <69964016+ayushil@users.noreply.github.com>
2020-10-17 23:59:19 +0530

    Update HelloWorld.cpp

--- Hello World Programs/HelloWorld.cpp
+++ Hello World Programs/HelloWorld.cpp
@@ -6,3 +6,10 @@ cout<<"Hello World Hacktoberfest2020";
 cout<<"Happy Coding....Happy Contributing";
 return 0;
 }
+//program in c
+#include <stdio.h>
+
+int main() {
+    printf("Hello World Hacktoberfest2020 \n");
+	return 0;
+}
