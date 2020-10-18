b411e66c37d610d9222c995358625b6e3bd7a68d
sahil100 <sahil.sh100@gmail.com>
2020-10-18 13:20:08 +0530

    Added mergesort and recursive binary search in python in algorithms

---
+++ Data Structures and Algorithms/Algorithms/python/RecursiveBinarySearch.py
@@ -0,0 +1,36 @@
+# Returns index of x in arr if present, else -1 
+def binarySearch (arr, l, r, x): 
+  
+    # Check base case 
+    if r >= l: 
+  
+        mid = l + (r - l) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+          
+        # If element is smaller than mid, then it  
+        # can only be present in left subarray 
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+  
+        # Else the element can only be present  
+        # in right subarray 
+        else: 
+            return binarySearch(arr, mid + 1, r, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
+
+
+arr=[int(x) for x in input("Enter the array elements : ").split()] # input list using comprehension
+x=int(input("Enter the number to be searched : "))
+n = len(arr); 
+result = binarySearch(arr, 0, len(arr)-1, x) 
+  
+if result != -1: 
+    print ("Element is present at index % d" % result) 
+else: 
+    print ("Element is not present in array") 
