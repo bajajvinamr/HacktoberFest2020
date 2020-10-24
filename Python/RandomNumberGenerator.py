a88a26646bb9fa7e229fc805fba1f44c658ce91f
aadyar882 <72991590+aadyar882@users.noreply.github.com>
2020-10-24 12:08:12 -0700

    Update RandomNumberGenerator.py
    added function that outputs a list of random numbers given a list length and a minimum value

--- Python/RandomNumberGenerator.py
+++ Python/RandomNumberGenerator.py
@@ -1,3 +1,13 @@
 import random
 n = random.random()
 print(n)
+#%%
+lis = []
+def listofrandomnumbers():
+    range_n = input("How many numbers do you want to generate?: ")
+    minval = input("What is the minimum value you want to generate?: ")
+    while len(lis) <= 10: 
+        lis.append(int(range_n)*random.random()+int(minval))
+        if len(lis) == 10 :
+            break
+    print(lis)
