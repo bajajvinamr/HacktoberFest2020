5e0bb5de002270fe2555fba22988bb737e52369e
sadhna59 <31662791+sadhna59@users.noreply.github.com>
2020-10-30 22:20:40 +0530

    hello world

---
+++ Hello World Programs/HelloWorldJava.java
@@ -0,0 +1,9 @@
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+
+// in java this stament is used to print on console
+system.out.println("Hello world");
+system.out.println("Welcome to Hactoberfest2020");
+	
+}
